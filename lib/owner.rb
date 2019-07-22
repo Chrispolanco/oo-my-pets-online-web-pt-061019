312c47c16335307637d744c24774ed42025850bc
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-22 00:14:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,18 @@
 class Owner
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name =name 
+    @@all = []
   end 
   
   def self.species
     self = "Human"
+  end 
+  
+  def self.all 
+    @@all
   end 
   
 end
