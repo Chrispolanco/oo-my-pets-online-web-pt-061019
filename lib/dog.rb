ea70135d38dd990ed4a9b02598de1df8763275a1
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-22 01:16:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,12 @@ class Dog
   attr_accessor :mood, :owner 
   attr_reader :name 
   
-  
+  @@all 
   def initialize(name, owner, mood = "nervous")
     @name = name 
     @owner = owner
     @mood = mood
+    @@all << self 
   end 
   
 end
