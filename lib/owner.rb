77199bef97f53a6a90e623987978ea60a910246a
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-22 00:17:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,9 @@
 class Owner
-  attr_reader :name, :species
+  attr_accessor :pets 
   @@all = []
   
-  def initialize(name)
-    @name =name 
+  def initialize(species)
+    @species =species 
     @@all = []
   end 
   
