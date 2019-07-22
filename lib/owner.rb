99085f95f319841ac15a12b93d298f710abee8bd
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-22 02:13:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ class Owner
   end 
   
   def list_pets
-    "I have #{self.dogs.count}, and #{self.cats.count}"
+    "I have #{self.dogs.count} dogs, and #{self.cats.count} cats"
   end 
   
 end
